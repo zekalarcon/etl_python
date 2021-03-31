be0ab0faf4efc34ba13e8328d8d615b0c0fcfbaf
zekalarcon <zekalarcon@gmail.com>
2021-03-30 23:50:12 -0300

    Ejercicio realizado

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -10,9 +10,9 @@ Programa creado para poner a prueba los conocimientos
 adquiridos durante la clase
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
-__version__ = "1.1"
+__author__ = "Ezequiel Alarcon"
+__email__ = "zekalarcon@gmail.com"
+__version__ = "1.0"
 
 import bonobo
 import time
@@ -21,7 +21,7 @@ import requests
 # Install bonobo
 #   pip3 install -U bonobo 
 # Crear archivo "dot"
-#   bonobo inspect --graph ejercicios_clase.py > ejercicios_clase.dot
+#   bonobo inspect --graph ejercicios_practica.py > ejercicios_practica.dot
 # Graphviz online
 #   http://dreampuf.github.io/GraphvizOnline/
 # Graphviz (dot) extension
@@ -32,13 +32,17 @@ def extract():
     # Realice un bucle que recorra una lista del 0 al 10 inclusive
     # En cada iteración de ese bucle realizar un "yield" del valor
     # tomado de la lista
-    yield 1
+    
+    for x in range(11):
+        yield x
 
 
-def transform(x):
+def transform(x:int):
     # Por cada número que ingrese a transform
     # multiplicarlo por 5
-    yield 1
+
+    result = x * 5
+    yield result
 
 
 def load(result):
@@ -48,6 +52,10 @@ def load(result):
     # o insertando a una base de datos a elección.
     # El objetivo es que quede almacenado en un archivo
     # o una base de datos la tabla del 5
+    
+    with open('resultados.txt', 'a') as fi:
+        fi.writelines(str(result) + '\n')
+
     print('Fin!')
 
 
